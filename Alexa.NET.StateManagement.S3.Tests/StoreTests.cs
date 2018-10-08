46bfac71e2cc7519684d2402482247d7c76cd394
Steven Pears <steven.pears@live.co.uk>
2018-10-08 08:50:25 +0100

    Update tests and storage code

--- Alexa.NET.StateManagement.S3.Tests/StoreTests.cs
+++ Alexa.NET.StateManagement.S3.Tests/StoreTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using Alexa.NET.Request;
@@ -6,6 +8,8 @@ using Amazon;
 using Amazon.Runtime;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NSubstitute;
 using Xunit;
 
@@ -16,19 +20,19 @@ namespace Alexa.NET.StateManagement.S3.Tests
         [Fact]
         public void ConstructorThrowsOnNullBucket()
         {
-            Assert.Throws<ArgumentNullException>(() => new S3PersistenceStore(null,Substitute.For<IAmazonS3>()));
+            Assert.Throws<ArgumentNullException>(() => new S3PersistenceStore(null, Substitute.For<IAmazonS3>()));
         }
 
         [Fact]
         public void ConstructorThrowsOnEmptyBucket()
         {
-            Assert.Throws<ArgumentNullException>(() => new S3PersistenceStore(string.Empty,Substitute.For<IAmazonS3>()));
+            Assert.Throws<ArgumentNullException>(() => new S3PersistenceStore(string.Empty, Substitute.For<IAmazonS3>()));
         }
 
         [Fact]
         public void ConstructorThrowsOnNullS3Client()
         {
-            Assert.Throws<ArgumentNullException>(() => new S3PersistenceStore("test",(IAmazonS3)null));
+            Assert.Throws<ArgumentNullException>(() => new S3PersistenceStore("test", (IAmazonS3)null));
         }
 
         [Fact]
@@ -42,7 +46,7 @@ namespace Alexa.NET.StateManagement.S3.Tests
                 .ReturnsForAnyArgs(c => new GetObjectResponse { HttpStatusCode = HttpStatusCode.NotFound });
 
             var request = DummyRequest();
-            var s3 = new S3PersistenceStore("xxx",mock);
+            var s3 = new S3PersistenceStore("xxx", mock);
             var result = await s3.Get<string>(request, "test");
             Assert.Null(result);
 
@@ -61,7 +65,7 @@ namespace Alexa.NET.StateManagement.S3.Tests
         }
 
         [Fact]
-        public async Task CheckS3CorrectKey()
+        public async Task GetCheckS3CorrectKey()
         {
             var mock = Substitute.For<IAmazonS3>();
             var bucketCheck = mock.DoesS3BucketExistAsync("xxx").Returns(true);
@@ -75,15 +79,127 @@ namespace Alexa.NET.StateManagement.S3.Tests
             var result = await s3.Get<string>(request, "test");
             Assert.Null(result);
 
-            await mock.Received(1).GetObjectAsync("xxx","abc123");
+            await mock.Received(1).GetObjectAsync("xxx", "abc123");
         }
 
+        [Fact]
+        public async Task SetCheckS3CorrectKey()
+        {
+            var mock = Substitute.For<IAmazonS3>();
+            var bucketCheck = mock.DoesS3BucketExistAsync("xxx").Returns(true);
 
+            var existenceCheck = mock
+                .GetObjectAsync(string.Empty, string.Empty)
+                .ReturnsForAnyArgs(c => new GetObjectResponse { HttpStatusCode = HttpStatusCode.NotFound });
+
+            var request = DummyRequest();
+            var s3 = new S3PersistenceStore("xxx", mock);
+            await s3.Set(request, "test", "wibble");
+
+            await mock.Received(1).GetObjectAsync("xxx", "abc123");
+        }
+
+        [Fact]
+        public async Task SetTwiceDoesSingleCheck()
+        {
+            var mock = Substitute.For<IAmazonS3>();
+            mock.DoesS3BucketExistAsync("xxx").Returns(true);
+
+            mock
+                .GetObjectAsync(string.Empty, string.Empty)
+                .ReturnsForAnyArgs(c => new GetObjectResponse { HttpStatusCode = HttpStatusCode.NotFound });
+
+            var request = DummyRequest();
+            var s3 = new S3PersistenceStore("xxx", mock);
+            await s3.Set(request, "test", "wibble");
+            await s3.Set(request, "test2", "wibble2");
+
+            await mock.Received(1).DoesS3BucketExistAsync("xxx");
+            await mock.Received(1).GetObjectAsync("xxx", "abc123");
+        }
+
+        [Fact]
+        public async Task SetPutsValueLocally()
+        {
+            var mock = Substitute.For<IAmazonS3>();
+            mock.DoesS3BucketExistAsync("xxx").Returns(true);
+
+            mock
+                .GetObjectAsync(string.Empty, string.Empty)
+                .ReturnsForAnyArgs(c => new GetObjectResponse { HttpStatusCode = HttpStatusCode.NotFound });
+
+            var request = DummyRequest();
+            var s3 = new S3PersistenceStore("xxx", mock);
+            await s3.Set(request, "test", "wibble");
+
+            var result = await s3.Get<string>(request, "test");
+            Assert.Equal("wibble", result);
+        }
+
+        [Fact]
+        public async Task SavePushesToS3()
+        {
+            var mock = Substitute.For<IAmazonS3>();
+            mock.DoesS3BucketExistAsync("xxx").Returns(true);
+
+            mock
+                .GetObjectAsync(string.Empty, string.Empty)
+                .ReturnsForAnyArgs(c => new GetObjectResponse { HttpStatusCode = HttpStatusCode.NotFound });
+
+            var request = DummyRequest();
+            var s3 = new S3PersistenceStore("xxx", mock);
+            await s3.Set(request, "test", "wibble");
+
+            mock.When(a => a.PutObjectAsync(Arg.Any<PutObjectRequest>())).Do(call =>
+            {
+                var put = call.Arg<PutObjectRequest>();
+                Assert.Equal("xxx", put.BucketName);
+                Assert.Equal("abc123", put.Key);
+                Assert.NotNull(put.InputStream);
+                using (var reader = new StreamReader(put.InputStream))
+                {
+                    var content = reader.ReadToEnd();
+                    var data = JsonConvert.DeserializeObject<JObject>(content);
+                    Assert.Equal("wibble", data.Value<string>("test"));
+                }
+            });
+
+            await s3.Save(request);
+        }
+
+        [Fact]
+        public async Task GetRetrievesS3()
+        {
+            var mock = Substitute.For<IAmazonS3>();
+            mock.DoesS3BucketExistAsync("xxx").Returns(true);
+
+            var oms = new MemoryStream();
+            var writer = new StreamWriter(oms);
+
+            var dictionary = new Dictionary<string, object> {{"test", "wibble"}};
+
+            writer.Write(JsonConvert.SerializeObject(dictionary));
+            writer.Flush();
+            oms.Seek(0, SeekOrigin.Begin);
+
+            mock
+                .GetObjectAsync(string.Empty, string.Empty)
+                .ReturnsForAnyArgs(c => new GetObjectResponse
+                {
+                    HttpStatusCode = HttpStatusCode.OK,
+                    ResponseStream = oms
+                });
+
+            var request = DummyRequest();
+            var s3 = new S3PersistenceStore("xxx", mock);
+            var result = await s3.Get<string>(request,"test");
+            Assert.Equal("wibble", result);
+        }
 
         private SkillRequest DummyRequest()
         {
             return new SkillRequest
-                { Context = new Context { System = new AlexaSystem { User = new User { UserId = "abc123" } } } };
+            { Context = new Context { System = new AlexaSystem { User = new User { UserId = "abc123" } } } };
         }
     }
 }
--- Alexa.NET.StateManagement.S3/S3PersistenceStore.cs
+++ Alexa.NET.StateManagement.S3/S3PersistenceStore.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using Alexa.NET.Request;
-using Amazon;
 using Amazon.S3;
-using Newtonsoft.Json.Linq;
+using Amazon.S3.Model;
+using Newtonsoft.Json;
 
 namespace Alexa.NET.StateManagement.S3
 {
-    public class S3PersistenceStore:IPersistenceStore
+    public class S3PersistenceStore : IPersistenceStore
     {
-        public IAmazonS3 S3Client { get; } 
+        public IAmazonS3 S3Client { get; }
         public string BucketName { get; }
 
         public string KeyPrefix { get; set; }
-        private ConcurrentDictionary<string,JObject> Local { get; } = new ConcurrentDictionary<string, JObject>();
+        private ConcurrentDictionary<string, Dictionary<string, object>> Local { get; } = new ConcurrentDictionary<string, Dictionary<string, object>>();
 
-        private bool BucketChecked = false;
+        private JsonSerializer Serializer { get; }
 
-        public S3PersistenceStore(string bucketName, IAmazonS3 s3Client)
+        private bool BucketChecked;
+
+        public S3PersistenceStore(string bucketName, IAmazonS3 s3Client, JsonSerializerSettings jsonSettings = null)
         {
             if (string.IsNullOrWhiteSpace(bucketName))
             {
@@ -29,21 +32,42 @@ namespace Alexa.NET.StateManagement.S3
 
             BucketName = bucketName;
             S3Client = s3Client ?? throw new ArgumentNullException(nameof(s3Client));
+            Serializer = jsonSettings == null ? JsonSerializer.CreateDefault() : JsonSerializer.Create(jsonSettings);
         }
 
         public async Task<T> Get<T>(SkillRequest request, string key)
         {
-            await BucketCheck();
             var s3Key = GetS3Key(request);
-            if (!Local.TryGetValue(key, out JObject values))
-            {
-                var newLocal = await GetFromS3(s3Key);
-                Local.GetOrAdd(key, newLocal ?? new JObject());
-            }
+            var values = await EnsureValues(s3Key);
 
-            return default(T);
+            return values.ContainsKey(key) && values[key] is T ? (T)values[key] : default(T);
         }
 
+        private async Task<Dictionary<string, object>> EnsureValues(string s3Key)
+        {
+            await BucketCheck();
+            if (!Local.TryGetValue(s3Key, out Dictionary<string, object> values))
+            {
+                var newLocal = await GetFromS3(s3Key);
+                values = Local.GetOrAdd(s3Key, newLocal ?? new Dictionary<string, object>());
+            }
+
+            return values;
+        }
+
+        public async Task Set<T>(SkillRequest request, string key, T value)
+        {
+            var s3Key = GetS3Key(request);
+            var values = await EnsureValues(s3Key);
+            if (values.ContainsKey(key))
+            {
+                values[key] = value;
+            }
+            else
+            {
+                values.Add(key, value);
+            }
+        }
         private async Task BucketCheck()
         {
             if (BucketChecked)
@@ -60,14 +84,17 @@ namespace Alexa.NET.StateManagement.S3
             throw new InvalidOperationException($"Bucket {BucketName} does not exist");
         }
 
-        private async Task<JObject> GetFromS3(string s3Key)
+        private async Task<Dictionary<string, object>> GetFromS3(string s3Key)
         {
             try
             {
-                var response  = await S3Client.GetObjectAsync(BucketName, s3Key);
+                var response = await S3Client.GetObjectAsync(BucketName, s3Key);
                 if (response.HttpStatusCode == HttpStatusCode.OK)
                 {
-
+                    using (var reader = new JsonTextReader(new StreamReader(response.ResponseStream)))
+                    {
+                        return Serializer.Deserialize<Dictionary<string, object>>(reader);
+                    }
                 }
 
                 return null;
@@ -83,14 +110,27 @@ namespace Alexa.NET.StateManagement.S3
             return request.Context.System.User.UserId;
         }
 
-        public Task Set<T>(SkillRequest request, string key, T value)
+        public async Task Save(SkillRequest request)
         {
-            throw new NotImplementedException();
-        }
+            var s3Key = GetS3Key(request);
+            if (Local.ContainsKey(s3Key))
+            {
+                var oms = new MemoryStream();
+                var writer = new StreamWriter(oms);
+                Serializer.Serialize(writer, Local[s3Key]);
+                writer.Flush();
 
-        public Task Save(SkillRequest request)
-        {
-            throw new NotImplementedException();
+
+                oms.Seek(0, SeekOrigin.Begin);
+
+                var putRequest = new PutObjectRequest
+                {
+                    BucketName = BucketName,
+                    Key = s3Key,
+                    InputStream = oms
+                };
+                await S3Client.PutObjectAsync(putRequest);
+            }
         }
     }
 }
